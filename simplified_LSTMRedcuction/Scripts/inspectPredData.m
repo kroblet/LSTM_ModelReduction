9ebdb5c30779c4fa5867db18c6b774a4d479b7b0
Ioannis Myrsinias <imyrsini@mathworks.com>
2023-07-26 19:16:27 +0100

    feat: create "inspectPedData" to visualize LSTM output

---
+++ simplified_LSTMRedcuction/Scripts/inspectPredData.m
@@ -0,0 +1,22 @@
+function inspectPredData(trainData)
+    ax2 = subplot(2,1,1);
+    ax3 = subplot(2,1,2);
+    
+    hold(ax2,'on')
+    hold(ax3,'on')
+    
+    ylabel(ax2, 'Output Torque');
+    ylabel(ax3, 'Output Velocity');
+    
+    for ix=1:length(trainData)
+        tqout = trainData{ix}(2,:);
+        wout = trainData{ix}(3,:);
+    
+        % plot resampled data
+        plot(ax2, tqout)
+        plot(ax3, wout)
+    
+    end
+    
+    hold(ax2,'off')
+    hold(ax3,'off')
