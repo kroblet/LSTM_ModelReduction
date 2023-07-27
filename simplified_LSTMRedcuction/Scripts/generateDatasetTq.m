4918d9f7b09235aef555fc23e610fde2ad20efe1
Ioannis Myrsinias <imyrsini@mathworks.com>
2023-07-27 11:05:30 +0100

    feat: increase Simulation scenarios including Step input and negative input

--- simplified_LSTMRedcuction/Scripts/generateDatasetTq.m
+++ simplified_LSTMRedcuction/Scripts/generateDatasetTq.m
@@ -1,10 +1,30 @@
 function generateDatasetTq(maxT, name, trainDir)
+    % Trapezoidal input
     time = [0 0.5 0.7 0.8 2];
     torque = [0 maxT maxT 0 0]; 
     sdata = Simulink.SimulationData.Dataset(torque');
     sdata{1}.Values.Time = time';
     sdata{1}.Name = name;
-    fileName = fullfile(trainDir, [name '.mat']);
-
+    fileName = fullfile(trainDir, [name '_trap' '.mat']);
     save(fileName, "sdata")
+
+    % Positive Step input
+    time = [0 0.5 1 1 2];
+    torque = [0 0 0 maxT maxT]; 
+    sdata = Simulink.SimulationData.Dataset(torque');
+    sdata{1}.Values.Time = time';
+    sdata{1}.Name = name;
+    fileName = fullfile(trainDir, [name '_posStep' '.mat']);
+    save(fileName, "sdata")
+
+    % Negative Step input
+    time = [0 0.5 1 1 2];
+    torque = [0 0 0 -maxT -maxT]; 
+    sdata = Simulink.SimulationData.Dataset(torque');
+    sdata{1}.Values.Time = time';
+    sdata{1}.Name = name;
+    fileName = fullfile(trainDir, [name '_negStep' '.mat']);
+    save(fileName, "sdata")
+
+    
 end
