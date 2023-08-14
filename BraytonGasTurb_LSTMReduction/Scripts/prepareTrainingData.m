900baa5fc2e0325313ef8685f0a9c5383dd81da4
Ioannis Myrsinias <imyrsini@mathworks.com>
2023-08-14 11:18:03 +0100

    feat: add T,P of every stage of the brighton cycle in the data for training

--- BraytonGasTurb_LSTMReduction/Scripts/prepareTrainingData.m
+++ BraytonGasTurb_LSTMReduction/Scripts/prepareTrainingData.m
@@ -14,23 +14,51 @@ for ix =1:caseNum
     compRPM = out(ix).logsout{1}.Values.Data;
     power = out(ix).logsout{5}.Values.Data;
 
-    % refTorq = out(ix).simlog.Motor_Drive.Tr.series.values; % reference torque input in the motor
-    % outTorque = out(ix).simlog.Inertia.t.series.values; % motor output torque
-    % outVelocity = out(ix).simlog.Inertia.w.series.values; % motor shaft speed
+    % temperature/ pressure at thermodynamic stage 1 - Compressor input
+    t1 = out(ix).simlog_sscfluids_brayton_cycle.Ts_1.Pressure_Temperature_Sensor_G.T.series.values;
+    p1 = out(ix).simlog_sscfluids_brayton_cycle.Ts_1.Pressure_Temperature_Sensor_G.P.series.values;
 
-    % resample data
+    % temperature/ pressure at thermodynamic stage 2 - Burner input
+    t2 = out(ix).simlog_sscfluids_brayton_cycle.Ts_2.Pressure_Temperature_Sensor_G.T.series.values;
+    p2 = out(ix).simlog_sscfluids_brayton_cycle.Ts_2.Pressure_Temperature_Sensor_G.P.series.values;
+
+    % temperature/ pressure at thermodynamic stage 3 - Turbine input   
+    t3 = out(ix).simlog_sscfluids_brayton_cycle.Ts_3.Pressure_Temperature_Sensor_G.T.series.values;
+    p3 = out(ix).simlog_sscfluids_brayton_cycle.Ts_3.Pressure_Temperature_Sensor_G.P.series.values;
+
+    % temperature/ pressure at thermodynamic stage 4 - APU input
+    t4 = out(ix).simlog_sscfluids_brayton_cycle.Ts_4.Pressure_Temperature_Sensor_G.T.series.values;
+    p4 = out(ix).simlog_sscfluids_brayton_cycle.Ts_4.Pressure_Temperature_Sensor_G.P.series.values;    
+
+    % temperature/ pressure at thermodynamic stage 5 - APU output
+    t5 = out(ix).simlog_sscfluids_brayton_cycle.Ts_5.Pressure_Temperature_Sensor_G.T.series.values;
+    p5 = out(ix).simlog_sscfluids_brayton_cycle.Ts_5.Pressure_Temperature_Sensor_G.P.series.values;  
+
+    % resample actuation signals
     resTime = 0:sampleStep:max(time);
     apu_w_res = interp1(time, apu_w, resTime);
     phi_res = interp1(time, phi, resTime);
     vn_res = interp1(time, vn, resTime);
+    vn_apu_res = interp1(time, vn_apu, resTime);
     compRPM_res = interp1(time, compRPM, resTime);
     power_res = interp1(time, power, resTime);
 
-    % refTorq_res = interp1(time, refTorq, resTime);    
-    % outTorque_res = interp1(time, outTorque, resTime);
-    % outVelocity_res = interp1(time, outVelocity, resTime);
+    % resample physical signals
+    t1_res = interp1(time, t1, resTime);
+    t2_res = interp1(time, t2, resTime);
+    t3_res = interp1(time, t3, resTime);
+    t4_res = interp1(time, t4, resTime);
+    t5_res = interp1(time, t5, resTime);
 
-    data{ix} = [apu_w_res;phi_res;vn_res;vn_apu_res;compRPM_res;power_res];
+    p1_res = interp1(time, p1, resTime);
+    p2_res = interp1(time, p2, resTime);
+    p3_res = interp1(time, p3, resTime);
+    p4_res = interp1(time, p4, resTime);
+    p5_res = interp1(time, p5, resTime);    
+
+    data{ix} = [apu_w_res;phi_res;vn_res;vn_apu_res;compRPM_res;power_res;...
+        t1_res;t2_res;t3_res;t4_res;t5_res;...
+        p1_res;p2_res;p3_res;p4_res;p5_res];
 
 end
 end
