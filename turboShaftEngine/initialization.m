0f8933bfdccc6afda8a347a6cc8e4d0cf0858459
Ioannis Myrsinias <imyrsini@mathworks.com>
2023-09-21 22:18:21 +0100

    fix: energy for the specifications of the GE T700 should be 1e3 KW bug: when FTP is connected mass flow is decreased (something like surge)

--- turboShaftEngine/initialization.m
+++ turboShaftEngine/initialization.m
@@ -1,27 +1,27 @@
 % initial conditions
 initConditions.Temperature = 288.15; % K
 initConditions.Pressure = 0.101325; % MPa
-initConditions.rpm = 7000; % initial shaft speed
+initConditions.rpm = 10000; % initial shaft speed
 
 % inlet - environment
-inlet.crossArea = 0.006;
+inlet.crossArea = 0.3;
 
 % exhaust - environment
-exhaust.crossArea = 0.005;
+exhaust.crossArea = 1*inlet.crossArea;
 
 % compressor
 compressor.areaRatio = 2; % fraction between inlet and outlet area
-compressor.rpmDesign = 9000; % rpm
-compressor.PR = 4;  % pressure ratio
-compressor.massFlowDesign = 1; % kg/s
+compressor.rpmDesign = 10000; % rpm
+compressor.PR = 17.5;  % pressure ratio
+compressor.massFlowDesign = 4; % kg/s
 compressor.isentropicEffMax = 0.887; % maximum isentropic efficiency
 compressor.isentropicEffMin = 0.5; % minimum isentropic efficiency
 compressor.massFlowMaxEff = 3; % maximum mass flow in max efficiency kg/s
 compressor.PRMaxEff = 4.5; % maximum PR in max efficiency 
-compressor.isentropicEfficiency = 0.8; % constant isentropic efficiency
+compressor.isentropicEfficiency = 0.85; % constant isentropic efficiency
 compressor.refPressure = initConditions.Pressure; % MPa
 compressor.refTemperature = initConditions.Temperature; % K
-compressor.mechanicalEff = 0.9; % mechanical efficiency
+compressor.mechanicalEff = 0.99; % mechanical efficiency
 compressor.inletArea = inlet.crossArea; % inlet area m2
 compressor.outletArea = compressor.inletArea/compressor.areaRatio; % outlet area m2
 % compressor.outletArea = 0.005; % outlet area m2
@@ -31,16 +31,28 @@ burner.length = 0.5; % m
 burner.crossArea = compressor.outletArea; % m2
 burner.diameter = 2*sqrt(burner.crossArea/pi); % m
 burner.hydrDiameter = burner.diameter; % cylindrical, same as diameter
+burner.initTemp = 900; % K
 
-% turbine
-turbine.PR = 3; % pressure ratio
-turbine.massFlowDesign = 1; % kg/s
-turbine.isentropicEfficiency = 0.8; % constant isentropic efficiency
+% turbine GGT
+turbine.areaRatio = 2; % fraction between inlet and outlet area
+turbine.PR = 4; % pressure ratio
+turbine.massFlowDesign = 4; % kg/s
+turbine.isentropicEfficiency = 0.85; % constant isentropic efficiency
 turbine.refPressure = initConditions.Pressure; % MPa
 turbine.refTemperature = initConditions.Temperature; % K
-turbine.mechanicalEff = 0.9; % mechanical efficiency
+turbine.mechanicalEff = 0.99; % mechanical efficiency
 turbine.inletArea = burner.crossArea; % inlet area m2
-turbine.outletArea = exhaust.crossArea; % outlet area m2
+turbine.outletArea = turbine.areaRatio*turbine.inletArea; % outlet area m2
+
+% turbine FPT
+turbineFPT.PR = 3.4; % pressure ratio
+turbineFPT.massFlowDesign = 4; % kg/s
+turbineFPT.isentropicEfficiency = 0.85; % constant isentropic efficiency
+turbineFPT.refPressure = initConditions.Pressure; % MPa
+turbineFPT.refTemperature = initConditions.Temperature; % K
+turbineFPT.mechanicalEff = 0.99; % mechanical efficiency
+turbineFPT.inletArea = turbine.outletArea; % inlet area m2
+turbineFPT.outletArea = exhaust.crossArea; % outlet area m2
 
 % shaft
 shaft.inertia = 1e-4; % kg*m2
