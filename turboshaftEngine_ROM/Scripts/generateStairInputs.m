c1903aa4f9c34ac7195afd28ae5a64f7f80985aa
imyrsini <imyrsini@mathworks.com>
2023-10-18 15:35:24 +0100

    refactor: move useful scripts to "turboShaftEngine_ROM/Scripts"

---
+++ turboshaftEngine_ROM/Scripts/generateStairInputs.m
@@ -0,0 +1,11 @@
+function generateStairInputs(scenariopDir, stateVec, time, idx, physMagnitude)
+%GENERATESTAIRINPUT function generates simulaiton inputs for stair
+%reference shaft velocity
+name = [physMagnitude '_' num2str(stateVec(1)) '_' num2str(stateVec(end))];
+stateVecRef = Simulink.SimulationData.Dataset(stateVec');
+stateVecRef{1}.Values.Time = time';
+stateVecRef{1}.Name = name;
+fileName = fullfile(scenariopDir, [num2str(idx) name '_st_' num2str(idx) '_' num2str(max(time)) '.mat']);
+save(fileName, "stateVecRef");
+end
+
