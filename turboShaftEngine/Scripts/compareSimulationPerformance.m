8a23ff7aa7019cd62fe12d4a479e7e1c30bdaa89
imyrsini <imyrsini@mathworks.com>
2023-10-19 14:53:08 +0100

    feat: compareSimulationPerformance

--- turboShaftEngine/Scripts/compareSimulationPerformance.m
+++ turboShaftEngine/Scripts/compareSimulationPerformance.m
@@ -2,8 +2,8 @@ function compareSimulationPerformance(refOut,romOut)
 %COMPARESIMULATIONS Summary of this function goes here
 %   Detailed explanation goes here
 figure
-ax1 = subplot(2,1,1)
-ax2 = subplot(2,1,2)
+ax1 = subplot(2,1,1);
+ax2 = subplot(2,1,2);
 
 ref.init = refOut.SimulationMetadata.TimingInfo.InitializationElapsedWallTime;
 ref.execution = refOut.SimulationMetadata.TimingInfo.ExecutionElapsedWallTime;
@@ -16,14 +16,14 @@ rom.total = romOut.SimulationMetadata.TimingInfo.TotalElapsedWallTime;
 reduced = [rom.init rom.execution rom.total];
 
 categories = categorical({'Initialization','Execution','Total'});
-bar(ax1, categories, [baseline;reduced])
-ylabel(ax1,'Time(s)')
+barh(ax1, categories, [baseline;reduced])
+xlabel(ax1,'Time (s)')
 legend(ax1, {'Simscape model', 'LSTM ROM'})
 grid(ax1,'on')
 
 timereduction = (baseline-reduced)./baseline.*100;
-bar(ax2, categories, timereduction)
-ylabel(ax2,'Time reduction %')
+barh(ax2, categories, timereduction, 0.3)
+xlabel(ax2,'Time reduction (%)')
 grid(ax2,'on')
 
 
