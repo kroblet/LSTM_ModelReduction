bc6045d0caa1d4bd0ec156db14977b1dd60bcfcf
imyrsini <imyrsini@mathworks.com>
2023-10-23 12:29:41 +0100

    refactor: change the "startup.m" file

--- startup.m
+++ startup.m
@@ -1,4 +1,4 @@
 proj = matlab.project.rootProject; % project root
-scenarioDir = fullfile(proj.RootFolder, 'Demo', 'SimulationInput');
-simOutDir = fullfile(proj.RootFolder, 'Demo', 'SimulationOutput');
-modelName = 'brayton_cycle_lstm_simplified';
+scenarioDir = fullfile(proj.RootFolder, 'turboshaftEngine', 'SimulationInput');
+simOutDir = fullfile(proj.RootFolder, 'turboshaftEngine', 'SimulationOutput');
+modelName = 'simpleHelicopter_toReduce';
