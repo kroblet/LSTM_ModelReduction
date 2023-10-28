51a4105bbb6f014185f7ea79261de05f3cd5094e
imyrsini <imyrsini@mathworks.com>
2023-10-28 22:23:34 +0100

    refactor: cleaning MLX for videos

--- turboShaftEngine/initialization.m
+++ turboShaftEngine/initialization.m
@@ -1,28 +1,18 @@
 % initial conditions
-fixedStepSize = 0.1;
-sampleTime = 0.1;
-Qin = [2000 2000 3100 3100 3600 3600 4100 4100 4600 4600 5100 5100 5100 5100];
-Qin_time = [0 66.6667 83.3333 150.0000 166.6667 233.3333 250.0000 316.6667 333.3333 400.0000 416.6667 483.3333 500.0000 566.6667];
-
-height = 0;
+fixedStepSize = 0.1; % s
+sampleTime = 0.1; % s
+height = 0; % m
+initConditions.rho = 1.2250; 
 g= 9.81; % m/s2 
-[T,a,P,rho,nu] = atmosisa(height);
+Qin = [2000 2000 3100 3100 3600 3600 4100 4100 4600 4600 5100 5100 5100 5100]; % kW
+Qin_time = [0 66.6667 83.3333 150.0000 166.6667 233.3333 250.0000 316.6667 333.3333 400.0000 416.6667 483.3333 500.0000 566.6667]; % s
 
-initConditions.Temperature = T; % K
-initConditions.Pressure = P; % Pa
-initConditions.rho = rho; % air density kg/m3
-initConditions.nu = nu; % kinematic viscocity m2/s
-initConditions.SounSpeed = a; % sound speed
-
-clear T a P rho nu;
-
-initConditions.rpm = 44700; % initial shaft speed
 
 % inlet - environment
 inlet.crossArea = 0.08;
 
 % exhaust - environment
-exhaust.crossArea = 1*inlet.crossArea;
+exhaust.crossArea = inlet.crossArea;
 
 % Global specs
 coreArea = 0.3;
@@ -38,20 +28,15 @@ compressor.refPressure = 101325; % Pa
 compressor.refTemperature = 288.15; % K
 compressor.massFlowDesign = 4.6122; % kg/s
 compressor.massFlowCorrected = compressor.massFlowDesign*sqrt((289.44+717.99)/2/compressor.refTemperature)/...
-    (95891/compressor.refPressure);
+    (95891/compressor.refPressure); % kg/s
 compressor.rpmDesign = 44700; % rpm
 compressor.rpmDesignCorrected = compressor.rpmDesign/sqrt((289.44+717.99)/2/compressor.refTemperature);
-
-
 compressor.mechanicalEff = 0.99; % mechanical efficiency
 compressor.inletArea = inlet.crossArea; % inlet area m2
 compressor.outletArea = compressor.inletArea/compressor.areaRatio; % outlet area m2
-
 compressor.isentropicEffMax = 0.887; % maximum isentropic efficiency
 compressor.isentropicEffMin = 0.5; % minimum isentropic efficiency
 compressor.massFlowMaxEff = 3; % maximum mass flow in max efficiency kg/s
-
-% compressor.outletArea = 0.005; % outlet area m2
 
 % burner
 burner.length = 0.5; % m
@@ -61,7 +46,6 @@ burner.hydrDiameter = burner.diameter; % cylindrical, same as diameter
 burner.initTemp = 1479; % K
 burner.initPress = 1591629; % Pa
 burner.eff = 0.9850; % combustion efficiency
-burner.pressureloss = 0.04*initConditions.Pressure;
 burner.HHV = 43100; % Higher heating value kJ/kg
 burner.mfDesign = 0.1004; % design fuel flow kg/s
 burner.heatDesign = burner.eff*burner.mfDesign*burner.HHV; % heat required for the design point
@@ -77,7 +61,7 @@ turbine.refTemperature = compressor.refTemperature; % K
 turbine.mechanicalEff = 0.99; % mechanical efficiency
 turbine.massFlowDesign = 1.025*compressor.massFlowDesign; % kg/s
 turbine.massFlowCorrected = compressor.massFlowDesign*sqrt((burner.initTemp+1125.08)/2/turbine.refTemperature)/...
-    ((burner.initPress+371631)/2/turbine.refPressure);
+    ((burner.initPress+371631)/2/turbine.refPressure); % kg/s
 
 % turbine FPT
 turbineFPT.PR = 4.3750; % pressure ratio
@@ -91,7 +75,7 @@ turbineFPT.RPMDesign = 20900; % design RPM for FTP
 turbineFPT.RotorDamping = 10; % kg*m^2
 turbineFPT.massFlowDesign = 1.025*compressor.massFlowDesign;
 turbineFPT.massFlowCorrected = turbineFPT.massFlowDesign*sqrt((884.09+1125.08)/2/turbine.refTemperature)/...
-    ((371631+109636)/2/turbine.refPressure);
+    ((371631+109636)/2/turbine.refPressure); % kg/s
 
 % nozzle
 nozzle.PR = 1.0142; % nozzle pressure ratio
@@ -102,7 +86,7 @@ nozzle.refTemperature = compressor.refTemperature; % K
 nozzle.inletArea = turbineFPT.outletArea; % inlet area m2
 nozzle.outletArea = exhaust.crossArea; % outlet area m2
 nozzle.massFlowCorrected = nozzle.massFlowDesign*sqrt(884.09/turbine.refTemperature)/...
-    ((109636+108105)/2/turbine.refPressure);
+    ((109636+108105)/2/turbine.refPressure); % kg/s
 nozzle.damping = 1e-3; % kg*m^2
 
 
@@ -112,6 +96,7 @@ rotorDamping = 1e-3; % kg*m^2
 shaft.inertia = 1e-4; % kg*m2
 shaft.damping = 1e-3; % N*m*s*rad 
 shaftDamping = 1e-5; % N*m*s*rad 
+initConditions.rpm = 44700; % initial shaft speed at design point
 
 % shaft FTP
 shaftFTP.GearRatio = 80.5;
@@ -125,8 +110,8 @@ rotor.inertia2blades = ...
 rotor.inertia = rotor.bladeNum/2 * rotor.inertia2blades;
 rotor.powerCoeff = 0.43; % rotor's power coefficient
 rotor.thrustCoeff = 0.8; % rotor's torque coefficient
-rotor.rpm = 257;
-rotor.area = pi*rotor.diameter^2/4;
+rotor.rpm = 257; % RPM
+rotor.area = pi*rotor.diameter^2/4; % m^2
 
 % chassis
 chassis.mass = 6000; % kg
@@ -152,16 +137,7 @@ rom.std.power = stdTrain(6);
 rom.std.T3 = stdTrain(7);
 rom.std.p2 = stdTrain(8);
 
-% rom.init.h = 1.5767;
-% rom.init.Qin = 1.1079;
-% rom.init.AirMassFlow = -2.1875;
-% rom.init.fptRPM = -1.7093;
-% rom.init.ggtRPM = -1.7263;
-% rom.init.power = -1.4515;
-% rom.init.T3 = -1.5059;
-% rom.init.p2 = -2.0145;
 rom.init.timeColdStart = 20;
-
 rom.init.h = -0.883502305729634;
 rom.init.Qin = -1.756277954474450;
 rom.init.AirMassFlow = -1.822791999032473;
