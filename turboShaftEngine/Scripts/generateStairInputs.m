8e057760aa867919dc1c999fb46bbd7191df545b
imyrsini <imyrsini@mathworks.com>
2023-11-07 12:22:04 +0000

    fix: mlx changes to avoid discontinuities in the data

--- turboShaftEngine/Scripts/generateStairInputs.m
+++ turboShaftEngine/Scripts/generateStairInputs.m
@@ -5,7 +5,7 @@ name = [physMagnitude '_' num2str(stateVec(1)) '_' num2str(stateVec(end))];
 stateVecRef = Simulink.SimulationData.Dataset(stateVec');
 stateVecRef{1}.Values.Time = time';
 stateVecRef{1}.Name = name;
-fileName = fullfile(scenariopDir, [num2str(idx) name '_st_' num2str(idx) '_' num2str(max(time)) '.mat']);
+fileName = fullfile(scenariopDir, [num2str(idx) name '_st_' num2str(idx) '_' num2str(max(time)-5) '.mat']);
 save(fileName, "stateVecRef");
 end
 
