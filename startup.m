30764033b827a68401ea181ed5a6c92c007d6fa2
imyrsini <imyrsini@mathworks.com>
2023-11-07 14:50:57 +0000

    fix: remove unused path and add more files to git

---
+++ startup.m
@@ -0,0 +1,4 @@
+proj = matlab.project.rootProject; % project root
+scenarioDir = fullfile(proj.RootFolder, 'turboshaftEngine', 'SimulationInput');
+simOutDir = fullfile(proj.RootFolder, 'turboshaftEngine', 'SimulationOutput');
+modelName = 'simpleHelicopter_toReduce';
