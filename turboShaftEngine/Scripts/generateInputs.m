fa502a2d170d45ec6427d60f950ee69881976916
imyrsini <imyrsini@mathworks.com>
2023-12-06 15:03:44 +0000

    feat: add "LSTM reduction of a Gas Turbine for Real-Time AIRBUS" presented in webinar for AIRBUS

--- turboShaftEngine/Scripts/generateInputs.m
+++ turboShaftEngine/Scripts/generateInputs.m
@@ -1,7 +1,7 @@
 function generateInputs(scenarioDir,states, simStopTime, idx, physMagn)
 %GENERATEINPUTS generates Simulation scenarios for a given vector
 %   Detailed explanation goes here
-stateVector = shaftSpeedVector(states);
+stateVector = stairVector(states);
 stepTimes = linspace(0,simStopTime, length(states));
 timeStep = (stepTimes(2)-stepTimes(1));
 time = timeVector(stepTimes, 0.8*timeStep);
