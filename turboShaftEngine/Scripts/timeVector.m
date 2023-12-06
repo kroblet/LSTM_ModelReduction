15940d8535b7eb7eadd61867964c163e832684c8
imyrsini <imyrsini@mathworks.com>
2023-12-06 10:44:47 +0000

    fix: add 'timeVector', and 'testScenario' in the repository

---
+++ turboShaftEngine/Scripts/timeVector.m
@@ -0,0 +1,10 @@
+function t = timeVector(time,stairOffset)
+
+    t = zeros(1,2*length(time));
+    for ix =1:2:numel(t)
+        idx = ceil(ix/2);
+        t(ix) = time(idx);
+        t(ix+1) = time(idx)+stairOffset;
+    end
+end
+
